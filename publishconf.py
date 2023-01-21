7141c6a440b4e38b0b95dd139d395231fbb60523
Seung-Yoon Oh <feynubrick@proton.me>
2023-01-21 20:53:13 +0900

    change theme to notmyidea

--- publishconf.py
+++ publishconf.py
@@ -1,10 +1,10 @@
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
+from pelicanconf import *
 import os
 import sys
 sys.path.append(os.curdir)
-from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://feynubrick.github.io'
@@ -15,7 +15,9 @@ CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
+THEME = 'notmyidea'
+
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
